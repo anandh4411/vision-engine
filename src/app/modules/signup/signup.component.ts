70fdccd124db98395392e8d1831ab905504a3bfb
anandh4411 <anandh4411@gmail.com>
2023-07-26 16:03:20 +0530

    added icons to input fields

--- src/app/modules/signup/signup.component.ts
+++ src/app/modules/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
@@ -7,8 +9,32 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
+  subject = new Subject<boolean>();
+  passwordHide = true;
+  cpasswordHide = true;
+  fileUploadQueue: any;
+
+  constructor(private modalService: NgbModal) {}
+
   emailFormControl = new FormControl('', [
     Validators.required,
     Validators.email,
   ]);
+
+  csvInputChange(fileInputEvent: any) {
+    console.log(fileInputEvent.target.files[0]);
+  }
+
+  exitPage(exit: boolean) {
+    if (exit) {
+      this.subject.next(true);
+      this.modalService.dismissAll();
+    } else {
+      this.subject.next(false);
+    }
+  }
+
+  open(exit: any) {
+    this.modalService.open(exit, { centered: true });
+  }
 }
