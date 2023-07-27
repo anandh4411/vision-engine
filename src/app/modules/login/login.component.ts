b28ec28adc36b31de9ce7688ffd9bcc83a84c4f8
anandh4411 <anandh4411@gmail.com>
2023-07-27 17:27:50 +0530

    finished login page, mobile responsiveness as well

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public isMobile: any;
+  loginMethodsActive: boolean = false;
+  passwordHide = true;
+  cpasswordHide = true;
 
+  constructor(private deviceService: DeviceDetectorService) {}
+
+  ngOnInit(): void {
+    this.isMobile = this.deviceService.isMobile();
+  }
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  changeMethod() {
+    this.loginMethodsActive = !this.loginMethodsActive;
+  }
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
-  declarations: [
-    LoginComponent
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
-  imports: [CommonModule],
 })
 export class LoginModule {}
