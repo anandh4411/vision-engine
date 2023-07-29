1c532d322b265f34fac48d2659ea8933646d25b1
anandh4411 <anandh4411@gmail.com>
2023-07-29 22:17:22 +0530

    started profile modal

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  constructor(private modalService: NgbModal) {}
 
+  open(modalName: any) {
+    this.modalService.open(modalName, { centered: true });
+  }
 }
