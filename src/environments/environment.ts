7558dc505bd9a888e798c7e23f475c43082c44be
anandh4411 <anandh4411@gmail.com>
2023-08-15 20:52:13 +0530

    changed vercel's url with render's deploy url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://vision-engine-auth-service.onrender.com/',
-  socketIoUrl:
-    'https://vision-engine-communication-service-q76fygmh6-anandh4411.vercel.app',
+  socketIoUrl: 'https://vision-engine-communication-service.onrender.com',
 };
